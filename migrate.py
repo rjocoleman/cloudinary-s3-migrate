f5e5a1bb41863244f96634aea86d1a57d14f5723
Robert Coleman <github@robert.net.nz>
2023-09-30 19:19:23 +1300

    add time to log output

--- migrate.py
+++ migrate.py
@@ -7,8 +7,11 @@ import logging
 
 # Set up logging
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
-logging.basicConfig(level=log_level)
-
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 # Cloudinary Configuration
 config = cloudinary.config(secure=True)
 
